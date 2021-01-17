9a0d50c73e8507a0f63391ec8b40a930d425c43e
vanshaj <himanshudhar6@gmail.com>
2021-01-18 00:27:44 +0530

    start code

---
+++ StringPractice/basics.java
@@ -0,0 +1,9 @@
+public class basics
+{
+	public static void main(String args[]){
+		System.out.println("hello world");
+	    	char[] c = {'j','a','v'};
+	    	String name = new String(c);
+	    	System.out.println(name);
+		}
+	}
