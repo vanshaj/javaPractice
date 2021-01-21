040150bd3569d5dd398e958e6dc4a6da6e667c23
Vanshaj <himanshudhar6@gmail.com>
2021-01-21 13:15:27 +0530

    Updated Race condtion 101

---
+++ multithreading/RaceCondition101.java
@@ -0,0 +1,81 @@
+class RaceCondition101{
+	public static void main(String[] args){
+			//SyncTwoSums two = new SyncTwoSums();
+			TwoSums two = new TwoSums();
+			
+			MyRunnable my1 = new MyRunnable(two,1,100000);
+			MyRunnable my2 = new MyRunnable(two,1,100000);
+			Thread myThread1 = new Thread(my1, "MyThread");
+			Thread myThread2 = new Thread(my2, "MyThread2");
+			myThread1.start();
+			myThread2.start();
+			try{
+			myThread1.join();
+			myThread2.join();
+
+			}
+			catch(Exception ex){
+				System.out.println("Exception caught"+ex);
+			}
+		}
+	}
+
+/*
+ * Non thread safe implementation 
+ * CRITICAL section is susceptible to Race condition
+ */
+
+class TwoSums{
+	 long sum2=0;
+
+	public void add(long val1, long val2){
+		for( long  i=val1; i<=val2; i++){
+			this.sum2++;					//CRITICAL SECTION
+			}
+		System.out.println("Async"+this.sum2);
+	}
+
+}
+
+/*
+ * Thread safe implementation of the read modify write condition
+ * Added atomicity only one thread can enter CRITICAL SECTION at a time
+ * Hence maintaining sync
+ */
+
+class SyncTwoSums{ 
+	long sum2=0;
+	
+	public void add(long x, long y){
+		for(long i = x; i<=y; i++){
+			synchronized (this) {
+				this.sum2++;		//CRITICAL SECTION
+			}
+		}
+		System.out.println("Sync"+this.sum2);
+	}
+	
+}
+
+class MyRunnable implements Runnable{
+	SyncTwoSums objS;
+	TwoSums obj;
+	long x,y;
+	
+	public MyRunnable(SyncTwoSums obj,long x, long y){ 	// USES THREAD SAFE CLASS
+		this.objS = obj;
+		this.x = x;
+		this.y = y;
+	}
+
+	public MyRunnable(TwoSums obj, long x, long y){  // USES NON THREAD SAFE CLASS
+		this.obj = obj;
+		this.x = x;
+		this.y = y;
+	}
+	
+	public void run(){
+			this.obj.add(this.x,this.y);
+	}
+}
+
